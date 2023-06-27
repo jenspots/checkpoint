588a2416e4d3ced15ae78dd2d5213f3651d8833f
Jens Pots <jens@jenspots.com>
2023-06-27 20:51:46 +0200

    fix: properly use env variables

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use actix_web::{web, App, HttpServer};
 use diesel::r2d2::ConnectionManager;
 use diesel::r2d2::Pool;
 use diesel::SqliteConnection;
+use dotenv::dotenv;
+use std::env;
 
 pub type ConnectionPool = Pool<ConnectionManager<SqliteConnection>>;
 
@@ -15,8 +17,12 @@ async fn main() -> std::io::Result<()> {
     // Configure logging.
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+    // Retrieve environment variables.
+    dotenv().ok();
+    let database_url = env::var("CHECKPOINT_DATABASE").expect("CHECKPOINT_DATABASE not set");
+
     // Use connection pool.
-    let manager = ConnectionManager::<SqliteConnection>::new("checkpoint.db");
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
     let pool = Pool::builder()
         .build(manager)
         .expect("Database URL should be valid path to SQLite DB file.");
